55d1d4d085d76fab4ca1be40cd460518094ae2a7
Christy <Edgardchristianne@gmail.com>
2024-08-07 15:16:37 -0400

    the pieces are on

--- Script.js
+++ Script.js
@@ -12,14 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {// set up an event listener
     const initialBoard = [ // this array reppresent the initiall layout of the chess board
         ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook'],
         ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
-        [], [], [], [],
+        [], [], [], [], // row  without pieces
         ['pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'],
         ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
     ];
 
+    // this function create a square for the chess board, give it color and adds piece
     const addPiece = (row, col, piece) => {
-        const square = document.createElement('div');
-        const colorClass = (row + col) % 2 === 0 ? 'white' : 'black';
+        const square = document.createElement('div'); // square is a new div element representing a single square
+        const colorClass = (row + col) % 2 === 0 ? 'green' : 'light green';
         square.className = colorClass;
         if (piece) {
             const pieceElement = document.createElement('span');
